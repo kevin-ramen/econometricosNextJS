b998f64438ad55cf761de72d227f2dd710402e6e
Michael Adolfo Huerta <michael.huerta@getechnologies.com.mx>
2023-12-01 18:21:48 -0600

    avances

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import geoJsonData from '../geojson/cdmx.geojson';
 
 const Index = () => (
   <div className="container mx-auto mt-8 text-center">
-    <h1 className="text-4xl font-bold mb-4">Explora los diferentes precios de las Tiendas</h1>
+    <h1 className="text-4xl font-bold mb-4">Explora los diferentes precios de las Tiendas!</h1>
     <div className="mt-8">
       <Mapa geoJsonData={geoJsonData} />
     </div>
