00ba9e800ba9fabfa751761af82b9e68afc28b24
Adolfo Huerta <fitox.metallica@gmail.com>
2023-12-07 20:33:52 -0600

    fin

--- components/header.js
+++ components/header.js
@@ -1,12 +1,22 @@
-// En tu componente de encabezado
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="bg-gradient-to-r from-teal-500 via-blue-500 to-purple-500 text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-bold">Buscador de locales comerciales</h1>
-        <p className="text-sm">Proyecto Final Modelos Econometricos</p>
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold">Buscador de locales comerciales</h1>
+          <p className="text-sm">Proyecto Final Modelos Econometricos</p>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link href="/about">
+            <p className="text-white">About Us</p>
+          </Link>
+          <Link href="/">
+            <p className="text-white">Regresar</p>
+          </Link>
+        </div>
       </div>
     </header>
   );
---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Header from "../components/header";
+import Footer from "../components/Footer";
+ 
+
+const About = () => {
+
+
+  return (
+    <>
+      <Header />
+            <p>
+                <b>¿Qué es el Mapa de la Comida?</b>
+            </p>
+            <p>
+                El Mapa de la Comida es un proyecto que busca ayudar a las personas a encontrar los mejores precios de locales de comida  en la Ciudad de México.
+            </p>
+            <p>
+                <b>¿Cómo funciona?</b>
+            </p>
+            <p>
+                El Mapa de la Comida utiliza datos abiertos de la Ciudad de México para mostrar los precios de locales de comida y supermercados en la ciudad.
+            </p>
+      <Footer />
+    </>
+  );
+};
+
+export default About;
