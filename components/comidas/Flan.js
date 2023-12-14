efdc30d55ee46fcdb6675bbe0aee2fb369dcb234
Kevin Ramírez Méndez <kevin.ramirez@timeweb.mx>
2023-12-13 23:49:21 -0600

    Agrega informacion e imagenes sobre Flan y Michelada

--- components/comidas/Flan.js
+++ components/comidas/Flan.js
@@ -2,10 +2,56 @@ import React from 'react'
 
 const Flan = () => {
     return (
-        <div>
-            <h1>Flan</h1>
+        <div className="max-w-4xl mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Flan</h1>
+            <p className="text-lg mb-2">Ingredientes:</p>
+            <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                    <thead>
+                        <tr className="bg-gray-100">
+                            <th className="px-6 py-3 text-left">PRODUCTO</th>
+                            <th className="px-6 py-3 text-left">PRESENTACIÓN</th>
+                            <th className="px-6 py-3 text-left">PRECIO PROMEDIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Aguacate} alt="Aguacate" className="w-8 h-8 mr-2" />
+                                AGUACATE
+                            </td>
+                            <td className="px-6 py-4">1 KG. HASS</td>
+                            <td className="px-6 py-4">43.41</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Carne} alt="Carne de res" className="w-8 h-8 mr-2" />
+                                CARNE RES
+                            </td>
+                            <td className="px-6 py-4">1 KG. GRANEL. MOLIDA ESPECIAL 80/20</td>
+                            <td className="px-6 py-4">96.67</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={tomate} alt="Tomate" className="w-8 h-8 mr-2" />
+                                JITOMATE
+                            </td>
+                            <td className="px-6 py-4">1 KG. SALADETTE/ HUAJE O TOMATE SALADETTE/ HUAJE</td>
+                            <td className="px-6 py-4">26.48</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={lechuga} alt="Lechuga" className="w-8 h-8 mr-2" />
+                                LECHUGA
+                            </td>
+                            <td className="px-6 py-4">PZA. ROMANA</td>
+                            <td className="px-6 py-4">16.31</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Flan
--- components/comidas/Michelada.js
+++ components/comidas/Michelada.js
@@ -1,11 +1,62 @@
 import React from 'react'
+import Cheve from './imagenes/cheve.png';
+import Limon from './imagenes/limon.png';
+import SalsaSoya from './imagenes/salsasoya.png';
+import SalsaInglesa from './imagenes/salsainglesa.png';
+import Image from 'next/image';
 
 const Michelada = () => {
     return (
-        <div>
-            <h1>Michelada</h1>
+        <div className="max-w-4xl mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Hamburguesa</h1>
+            <p className="text-lg mb-2">Ingredientes:</p>
+            <div className="overflow-x-auto">
+                <table className="min-w-full bg-white border border-gray-300">
+                    <thead>
+                        <tr className="bg-gray-100">
+                            <th className="px-6 py-3 text-left">PRODUCTO</th>
+                            <th className="px-6 py-3 text-left">PRESENTACIÓN</th>
+                            <th className="px-6 py-3 text-left">PRECIO PROMEDIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Cheve} alt="Cheve" className="w-8 h-8 mr-2" />
+                                CERVEZA
+                            </td>
+                            <td className="px-6 py-4">1.2 Lt.</td>
+                            <td className="px-6 py-4">43.11</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={Limon} alt="Limon" className="w-8 h-8 mr-2" />
+                                LIMON
+                            </td>
+                            <td className="px-6 py-4">1 Kg.</td>
+                            <td className="px-6 py-4">49.10</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={SalsaSoya} alt="SalsaSoya" className="w-8 h-8 mr-2" />
+                                SALSA DE SOYA
+                            </td>
+                            <td className="px-6 py-4">290 ml.</td>
+                            <td className="px-6 py-4">16.83</td>
+                        </tr>
+                        <tr>
+                            <td className="flex items-center px-6 py-4">
+                                <Image src={SalsaInglesa} alt="SalsaInglesa" className="w-8 h-8 mr-2" />
+                                SALSA INGLESA
+                            </td>
+                            <td className="px-6 py-4">290 ml.</td>
+                            <td className="px-6 py-4">65.45</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Michelada
